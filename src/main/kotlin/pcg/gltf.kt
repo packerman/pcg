d87f34e789c7a28505bb8efbca5cf13e68414df5
Piotr Ackermann <pioacker@gmail.com>
2019-09-03 22:41:17 +0200

    Validation

--- src/main/kotlin/pcg/gltf.kt
+++ src/main/kotlin/pcg/gltf.kt
@@ -2,18 +2,41 @@ package pcg
 
 import com.google.gson.GsonBuilder
 
+fun <T> requireNotEmpty(list: List<T>, name: String) =
+    require(list.isNotEmpty()) { "'$name' has to be not empty" }
+
+fun requireNonNegative(n: Int, name: String) =
+    require(n >= 0) { "'$name' has to be non-negative" }
+
 data class Gltf(
-    val asset: Asset
-)
+    val asset: Asset,
+    val scene: Int? = null,
+    val scenes: List<Scene>? = null
+) {
+
+    init {
+        scene?.let { requireNonNegative(it, "scene") }
+        scenes?.let { requireNotEmpty(it, "scenes") }
+    }
+}
 
 data class Asset(
     val version: String,
-    val minVersion: String?
+    val minVersion: String? = null
 )
 
+data class Scene(
+    val nodes: List<Int>? = null,
+    val name: String? = null
+) {
+
+    init {
+        nodes?.let { requireNotEmpty(it, "nodes") }
+    }
+}
+
 val defaultAsset = Asset(
-    version = "2.0",
-    minVersion = "2.0"
+    version = "2.0"
 )
 
 fun Gltf.toJson(prettyPrinting: Boolean = false): String {
@@ -27,7 +50,11 @@ fun Gltf.toJson(prettyPrinting: Boolean = false): String {
 
 fun main() {
     val gltf = Gltf(
-        asset = defaultAsset
+        asset = defaultAsset,
+        scene = 0,
+        scenes = listOf(
+            Scene(nodes = listOf(0))
+        )
     )
-    println(gltf.toJson())
+    println(gltf.toJson(true))
 }
