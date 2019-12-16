61eccb544781549a287d56e8a7d8de8304cff8d3
Piotr Ackermann <pioacker@gmail.com>
2019-12-17 00:00:11 +0100

    Stairs2

--- src/main/kotlin/pcg/example/stairs2.kt
+++ src/main/kotlin/pcg/example/stairs2.kt
@@ -6,7 +6,9 @@ import pcg.gltf.writeToFile
 import pcg.scene.*
 
 fun stairs2(
-    height: Float, length: Float, width: Float, steps: Int
+    height: Float, length: Float, width: Float,
+    thickness: Float,
+    steps: Int
 ): Geometry {
     require(steps >= 1)
     val x0 = 0f
@@ -31,12 +33,18 @@ fun stairs2(
             }
 
             leftSideIndex = currentCount
+            add(x0, y0, z0 - thickness)
+            add(x0, y0 + height - thickness, z0 - length)
+
             add(x0, y0, z0)
             for (s in 0 until steps) {
                 add(x0, y0 + dy * (s + 1), z0 + dz * s)
                 add(x0, y0 + dy * (s + 1), z0 + dz * (s + 1))
             }
+
             rightSideIndex = currentCount
+            add(x0 + width, y0, z0 - thickness)
+            add(x0 + width, y0 + height - thickness, z0 - length)
             add(x0 + width, y0, z0)
             for (s in 0 until steps) {
                 add(x0 + width, y0 + dy * (s + 1), z0 + dz * s)
@@ -55,12 +63,17 @@ fun stairs2(
                 add(0f, 1f, 0f)
                 add(0f, 1f, 0f)
             }
+            add(-1f, 0f, 0f)
+            add(-1f, 0f, 0f)
 
             add(-1f, 0f, 0f)
             for (s in 0 until steps) {
                 add(-1f, 0f, 0f)
                 add(-1f, 0f, 0f)
             }
+
+            add(1f, 0f, 0f)
+            add(1f, 0f, 0f)
 
             add(1f, 0f, 0f)
             for (s in 0 until steps) {
@@ -77,13 +90,17 @@ fun stairs2(
                 add(8 * s + 5, 8 * s + 6, 8 * s + 7)
             }
             leftSideIndex?.let { index ->
+                add(index, index + 2, index + 1)
+                add(index + 1, index + 2, index + 2 * steps + 2)
                 for (s in 0 until steps) {
-                    add(index + 2 * s, index + 2 * s + 2, index + 2 * s + 1)
+                    add(index + 2 * s + 2, index + 2 * s + 4, index + 2 * s + 3)
                 }
             }
             rightSideIndex?.let { index ->
+                add(index, index + 1, index + 2)
+                add(index + 1, index + 2 * steps + 2, index + 2)
                 for (s in 0 until steps) {
-                    add(index + 2 * s, index + 2 * s + 1, index + 2 * s + 2)
+                    add(index + 2 * s + 2, index + 2 * s + 3, index + 2 * s + 4)
                 }
             }
         }
@@ -94,7 +111,11 @@ fun main() {
     val s = scene {
         node(
             stairs2(
-                10f, 20f, 10f, 8
+                height = 10f,
+                length = 20f,
+                width = 10f,
+                thickness = 2f,
+                steps = 8
             )
         ) {
             material(Material(twoSided = true))
