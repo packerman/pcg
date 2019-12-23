f48cf1660f2febd33deb15bbdd34b4eee864b9ea
Piotr Ackermann <pioacker@gmail.com>
2019-12-23 22:38:07 +0100

    Box

---
+++ src/main/kotlin/pcg/example/box.kt
@@ -0,0 +1,91 @@
+package pcg.example
+
+import org.joml.Vector3f
+import org.joml.Vector3fc
+import pcg.compile.CompileOptions
+import pcg.compile.compile
+import pcg.gltf.writeToFile
+import pcg.scene.Attribute.Normal
+import pcg.scene.Attribute.Position
+import pcg.scene.Float3VertexArray.Companion.Float3VertexArrayBuilder
+import pcg.scene.Geometry
+import pcg.scene.ShortIndexArray.Companion.ShortIndexArrayBuilder
+import pcg.scene.oneMeshGeometry
+import pcg.scene.scene
+import pcg.util.Point3fc
+
+class Box(
+    o: Point3fc, a: Vector3fc, b: Vector3fc, c: Vector3fc,
+    m: Int = 1, n: Int = 1, p: Int = 1,
+    front: Boolean = true, back: Boolean = true,
+    left: Boolean = true, right: Boolean = true,
+    top: Boolean = true, bottom: Boolean = true
+) {
+
+    private val frontWall: Plane? = if (front) Plane(o, a, b, m, n) else null
+    private val rightWall: Plane? = if (right) Plane(o.add(a, Vector3f()), c, b, p, n) else null
+    private val backWall: Plane? = if (back) Plane(Vector3f(o).add(a).add(c), a.negate(Vector3f()), b, m, n) else null
+    private val leftWall: Plane? = if (left) Plane(o.add(c, Vector3f()), c.negate(Vector3f()), b, p, n) else null
+    private val topWall: Plane? = if (top) Plane(o.add(b, Vector3f()), a, c, m, p) else null
+    private val bottomWall: Plane? = if (bottom) Plane(o.add(c, Vector3f()), a, c.negate(Vector3f()), m, p) else null
+
+    private val walls: List<Plane> =
+        sequenceOf(frontWall, rightWall, backWall, leftWall, topWall, bottomWall).filterNotNull().toList()
+
+    fun provideVertices(builder: Float3VertexArrayBuilder): Unit = with(builder) {
+        walls.forEach { wall ->
+            wall.provideVertices(this)
+        }
+    }
+
+    fun provideNormals(builder: Float3VertexArrayBuilder): Unit = with(builder) {
+        walls.forEach { wall ->
+            wall.provideNormals(this)
+        }
+    }
+
+    fun provideIndices(builder: ShortIndexArrayBuilder, offset: Int = 0) = with(builder) {
+        var localOffset = offset
+
+        walls.forEach { wall ->
+            wall.provideIndices(builder, localOffset)
+            localOffset += wall.vertexCount
+        }
+    }
+}
+
+fun box(
+    o: Point3fc, a: Vector3fc, b: Vector3fc, c: Vector3fc,
+    m: Int = 1, n: Int = 1, p: Int = 1
+): Geometry {
+    val box = Box(o, a, b, c, m, n, p)
+
+    return oneMeshGeometry {
+        vertexArray3f(attribute = Position) {
+            box.provideVertices(this)
+        }
+        vertexArray3f(attribute = Normal) {
+            box.provideNormals(this)
+        }
+        indexArray {
+            box.provideIndices(this)
+        }
+    }
+}
+
+fun main() {
+    val s = scene {
+        node(
+            box(
+                o = Vector3f(0f, 0f, 0f),
+                a = Vector3f(10f, 0f, 0f),
+                b = Vector3f(0f, 10f, 0f),
+                c = Vector3f(0f, 0f, -10f),
+                m = 4, n = 4, p = 4
+            )
+        ) {
+            material()
+        }
+    }
+    writeToFile("TestPlane.gltf", compile(s, CompileOptions(interleaved = true)))
+}
--- src/main/kotlin/pcg/example/plane.kt
+++ src/main/kotlin/pcg/example/plane.kt
@@ -7,38 +7,62 @@ import pcg.compile.compile
 import pcg.gltf.writeToFile
 import pcg.scene.Attribute.Normal
 import pcg.scene.Attribute.Position
+import pcg.scene.Float3VertexArray.Companion.Float3VertexArrayBuilder
 import pcg.scene.Geometry
+import pcg.scene.ShortIndexArray.Companion.ShortIndexArrayBuilder
 import pcg.scene.oneMeshGeometry
 import pcg.scene.scene
 import pcg.util.Point3f
+import pcg.util.Point3fc
+
+class Plane(
+    private val origin: Point3fc, private val axis1: Vector3fc, private val axis2: Vector3fc,
+    private val m: Int = 1, private val n: Int = 1
+) {
+
+    val vertexCount = (m + 1) * (n + 1)
+
+    fun provideVertices(builder: Float3VertexArrayBuilder) = with(builder) {
+        val steps1: List<Vector3fc> = (0..m).map { i -> origin.lerp(axis1, i.toFloat() / m, Vector3f()) }
+        val steps2: List<Vector3fc> = (0..n).map { j -> origin.lerp(axis2, j.toFloat() / n, Vector3f()) }
+        for (i in 0..m) {
+            for (j in 0..n) {
+                vertex(Point3f(origin).add(steps1[i]).add(steps2[j]))
+            }
+        }
+    }
+
+    fun provideNormals(builder: Float3VertexArrayBuilder) = with(builder) {
+        val normal: Vector3fc = axis1.cross(axis2, Vector3f()).normalize()
+        repeat((m + 1) * (n + 1)) { vertex(normal) }
+    }
+
+    fun provideIndices(builder: ShortIndexArrayBuilder, offset: Int = 0) = with(builder) {
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                square(
+                    j + i * (n + 1) + offset, j + (i + 1) * (n + 1) + offset,
+                    j + (i + 1) * (n + 1) + 1 + offset, j + i * (n + 1) + 1 + offset
+                )
+            }
+        }
+    }
+}
 
 fun plane(
     origin: Vector3fc, axis1: Vector3fc, axis2: Vector3fc,
     m: Int = 1, n: Int = 1
 ): Geometry {
-    val steps1: List<Vector3fc> = (0..m).map { i -> origin.lerp(axis1, i.toFloat() / m, Vector3f()) }
-    val steps2: List<Vector3fc> = (0..n).map { j -> origin.lerp(axis2, j.toFloat() / n, Vector3f()) }
+    val plane = Plane(origin, axis1, axis2, m, n)
     return oneMeshGeometry {
         vertexArray3f(attribute = Position) {
-            for (i in 0..m) {
-                for (j in 0..n) {
-                    vertex(Point3f(origin).add(steps1[i]).add(steps2[j]))
-                }
-            }
+            plane.provideVertices(this)
         }
-        val normal: Vector3fc = axis1.cross(axis2, Vector3f()).normalize()
         vertexArray3f(attribute = Normal) {
-            repeat((m + 1) * (n + 1)) { vertex(normal) }
+            plane.provideNormals(this)
         }
         indexArray {
-            for (i in 0 until m) {
-                for (j in 0 until n) {
-                    square(
-                        j + i * (n + 1), j + (i + 1) * (n + 1),
-                        j + (i + 1) * (n + 1) + 1, j + i * (n + 1) + 1
-                    )
-                }
-            }
+            plane.provideIndices(this)
         }
     }
 }
